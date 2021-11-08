e6045f8e0760144cabf22f12467c0591d46ab1c1
liyanjie <jackelyj@gmail.com>
2021-11-08 21:06:32 +0800

    add SingletonLazy comments

--- src/main/java/singleton/SingletonLazy.java
+++ src/main/java/singleton/SingletonLazy.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class SingletonLazy
 {
+
+    // volatile 防止指令重排序
     private volatile static SingletonLazy instance = null;
 
     private List<String> list = null;
@@ -17,6 +19,8 @@ public class SingletonLazy
         if (null == instance) {
             synchronized (SingletonLazy.class) {
                 if (null == instance) {
+                    // 编译后指令：1. 分配对象的内存空间 2. 初始化对象 3. 将变量 instance 指向刚分配的内存空间
+                    // 使用 volatile 后，避免其他线程拿到未初始化的 SingletonLazy 对象
                     instance = new SingletonLazy();
                 }
             }
